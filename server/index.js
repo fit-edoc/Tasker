42fc421abe557d2d0660e18f9c32cef19bba26e3
fitman <fitmanverma@gmail.com>
2026-02-17 14:41:03 +0530

    add cors localhost

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors(
     {
-        origin: '*',
+        origin: ["https://localhost:3000"],
         credentials: true,
     }
 ));
