048f0c4c0e0d072f0e58e217c72f3f5bcdc27afe
fitman <fitmanverma@gmail.com>
2026-02-17 14:46:56 +0530

    cors policy

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://tasker-9tlq.onrender.com',
+    baseURL: 'https://tasker-9tlq.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
